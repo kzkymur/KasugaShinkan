5a2a475e9d40d1cc9d1d77f1048cb4da5ad9dfcf
山浦一輝 <yamaurakazuki@yamaurakazukinoMacBook-Pro.local>
2020-04-07 17:29:38 +0900

    fix wsgi

--- KasugaShinkan/wsgi.py
+++ KasugaShinkan/wsgi.py
@@ -7,9 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import site
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+site.addsitedir("/home/ubuntu/Django/env/lib/python3.6/site-packages")
+sys.path.append("/home/ubuntu/Django/KasugaShinkan")
+sys.path.append("/home/ubuntu/Django/KasugaShinkan/KasugaShinkan")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KasugaShinkan.settings')
 
