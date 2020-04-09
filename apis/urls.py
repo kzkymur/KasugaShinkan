250fd34a3aff274eb418e9cfbab9686afb7bc094
山浦一輝 <yamaurakazuki@yamaurakazukinoMacBook-Pro.local>
2020-04-09 17:21:34 +0900

    add question model

--- apis/urls.py
+++ apis/urls.py
@@ -7,5 +7,5 @@ app_name = 'apis'
 urlpatterns = [
     url(r'^topic/$', TopicApiView.as_view()),
     url(r'^manage_topic/$', TopicManageApiView.as_view()),
-    url(r'^question/$', QuestionApiView()),
+    url(r'^question/$', QuestionApiView.as_view()),
 ]
